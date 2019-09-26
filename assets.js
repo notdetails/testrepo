b0274904926b0178ec39c29e8de79de08f260842
Joel Califa <califa@github.com>
2019-09-26 18:25:40 -0400

    Create assets.js

---
+++ assets.js
@@ -0,0 +1,23 @@
+/* @flow */
+
+import { ASSET_TYPES } from 'shared/constants'
+import { isPlainObject, validateComponentName } from '../util/index'
+
+export function initAssetRegisters (Vue: GlobalAPI) {
+  /**
+   * Create asset registration methods.
+   */
+  ASSET_TYPES.forEach(type => {
+    Vue[type] = function (
+      id: string,
+      definition: Function | Object
+    ): Function | Object | void {
+      if (!definition) {
+        return this.options[type + 's'][id]
+      } else {
+        this.options[type + 's'][id] = definition
+        return definition
+      }
+    }
+  })
+}
